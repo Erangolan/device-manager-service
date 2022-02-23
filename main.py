f7652229fe034001b8f74f0a91eee3de874e0e3e
Erangolan <37651948+Erangolan@users.noreply.github.com>
2022-02-23 21:56:24 +0200

    added devices route

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from db import app
+from flask import jsonify, make_response
+from db import Device, app
 
 
 @app.route('/devices', methods=['GET'])
 def devices():
-    return True
+    validDevices = []
+    devices = Device.objects()
+    for device in devices:
+        if device['deleted'] == "False":
+            validDevices.append(device)
+
+    return make_response(jsonify(validDevices), 200)
 
 
 if __name__ == '__main__':
